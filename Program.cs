5f1b04c022db4805313883bc4f2aaf40a2850b1d
Hong-Yi Wang <wanghy2007@gmail.com>
2022-08-01 22:29:13 -0700

    Add movies controller

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
 // Add services to the container.
 
