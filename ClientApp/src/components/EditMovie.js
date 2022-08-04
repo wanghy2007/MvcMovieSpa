35ecc2ed55df2a86af0fad40f8189260e542b425
Hong-Yi Wang <wanghy2007@gmail.com>
2022-08-04 11:06:08 -0700

    Finish EditMovie.js

--- ClientApp/src/components/EditMovie.js
+++ ClientApp/src/components/EditMovie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { NavLink } from 'reactstrap';
 
 export function EditMovie() {
     const { id } = useParams();
@@ -25,11 +26,12 @@ export function EditMovie() {
     const handleChange = (event) => {
         const { name, value } = event.target;
         console.log(event.target);
+        console.log(name);
         console.log(value);
         setFormValues((prevState) => {
             return {
                 ...prevState,
-                name: value,
+                [name]: value,
             };
         });
     };
@@ -37,7 +39,8 @@ export function EditMovie() {
     const handleSubmit = async (event) => {
         event.preventDefault();
         await fetch(`movies/${id}`, {
-            method: "POST",
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 id: formValues.id,
                 title: formValues.title,
@@ -48,8 +51,11 @@ export function EditMovie() {
         });
     };
 
+    if (loading)
+      return <p><em>Loading...</em></p>
+
     return (
-        <div>
+        <>
             <h1>Edit</h1>
             <h4>Movie</h4>
             <hr/>
@@ -58,12 +64,28 @@ export function EditMovie() {
                     <form onSubmit={handleSubmit}>
                         <input type="hidden" value={formValues.id} onChange={handleChange} />
                         <div className="form-group">
-                            <label for="Title" className="control-label">Title</label>
-                            <input id="Title" className="form-control" value={formValues.title} onChange={handleChange} />
+                            <label htmlFor="title" className="control-label">Title</label>
+                            <input id="title" name="title" className="form-control" value={formValues.title} onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="releaseDate" className="control-label">Release Date</label>
+                            <input id="releaseDate" name="releaseDate" className="form-control" value={formValues.releaseDate} onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="genre" className="control-label">Genre</label>
+                            <input id="genre" name="genre" className="form-control" value={formValues.genre} onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="price" className="control-label">Price</label>
+                            <input id="price" name="price" className="form-control" value={formValues.price} onChange={handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <input type="submit" value="Save" className="btn btn-primary" />
                         </div>
                     </form>
                 </div>
+                <NavLink tag={Link} to={`/list-movie`}>Back to list</NavLink>
             </div>
-        </div>
+        </>
     );
 }
